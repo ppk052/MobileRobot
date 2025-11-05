4f6a70a0f323b757aaff112e8662cf6b033ee97f
MongNam <ppk052@naver.com>
2025-11-06 05:05:05 +0900

    setting temi

--- app/src/main/java/com/example/mobilerobot/MainActivity.java
+++ app/src/main/java/com/example/mobilerobot/MainActivity.java
@@ -1,19 +1,22 @@
 package com.example.mobilerobot;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.robotemi.sdk.listeners.OnRobotReadyListener;
+
+public class MainActivity extends AppCompatActivity /*implements
+        OnRobotReadyListener,
+        View.OnClickListener */{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -21,4 +24,24 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+//    @Override
+//    public void onRobotReady(boolean isReady){
+//
+//    }
+//
+//    @Override
+//    public void onClick(View view){
+//
+//    }
 }
