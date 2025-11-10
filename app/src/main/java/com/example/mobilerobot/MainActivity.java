7090cfcc94824f10a861f06a3cb31b3759276de3
MongNam <ppk052@naver.com>
2025-11-10 21:47:32 +0900

    개발중

--- app/src/main/java/com/example/mobilerobot/MainActivity.java
+++ app/src/main/java/com/example/mobilerobot/MainActivity.java
@@ -8,11 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.robotemi.sdk.Robot;
+import com.robotemi.sdk.TtsRequest;
 import com.robotemi.sdk.listeners.OnRobotReadyListener;
 
 public class MainActivity extends AppCompatActivity /*implements
         OnRobotReadyListener,
-        View.OnClickListener */{
+        View.OnClickListener */
+{
+    Robot robot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,4 +48,22 @@ public class MainActivity extends AppCompatActivity /*implements
 //    public void onClick(View view){
 //
 //    }
+
+    //메서드
+    //temiSDK 문서 : https://github.com/robotemi/sdk/wiki
+    public void temiMethod() {
+
+        // 사람 따라다니게하는 명령어
+        robot.beWithMe();
+
+        // 모든 움직임을 멈추는 명령어
+        robot.stopMovement();
+
+        //테미가 해당 글을 읽는 명령어
+        //create의 두번째 인자는 말할때 대화창을 표시하는지의 여부
+        String speak = "";
+        TtsRequest ttsRequest = TtsRequest.create(speak,true, TtsRequest.Language.KO_KR);
+        robot.speak(ttsRequest);
+
+    }
 }
